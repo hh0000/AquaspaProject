ecf55afa8ae5d9e505d85ed0c1e374dabe9b1085
Oscar Montecinos <o.montecinos.vasquez@gmail.com>
2019-06-07 19:28:49 -0400

    Formulario contacto

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,10 +4,13 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
         <title>Novasoft - Web manager</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
         <!-- Styles -->
         <style>
@@ -85,10 +88,64 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Contactenos</a>
-                   
+                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalContacto">
+                        Contactenos
+                    </button>                  
                 </div>
             </div>
         </div>
+
+        <!-- Modal -->
+<div class="modal fade" id="modalContacto" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Formulario de contacto</h5>           
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">    
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <div class="container">
+        <h6><small>Por favor, ingrese todos los datos solicitados</small></h6>
+        </div>
+
+            <div class="form-group">
+             <label for="usr">Nombre:</label>
+            <input type="text" class="form-control" id="usr">
+            </div>
+
+            <div class="form-group">
+             <label for="usr">Teléfono:</label>
+            <input type="text" class="form-control" id="usr">
+            </div>
+
+            <div class="form-group">
+            <label for="pwd">Mail:</label>
+            <input type="password" class="form-control" id="pwd">
+            </div> 
+
+            <div class="form-group">
+            <label for="comment">Comentarios:</label>
+            <textarea class="form-control" rows="5" id="comment"></textarea>
+            </div> 
+
+            <p class="text-break">2019 © Novasoft Chile,  All Rights Reserved. </p>
+
+        </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+        <button type="button" class="btn btn-primary">Enviar</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+
     </body>
 </html>
